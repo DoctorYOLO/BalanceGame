3d5f8be71d40737ff8c3256112314c04345ed3ed
Alexander <dell-95@mail.ru>
2017-05-04 20:11:12 +0300

    Replace Unity Master server with Photon Networking Framework

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,44 +3,38 @@ using System.Collections;
 
 public class MainMenu : MonoBehaviour 
 {
-
 	private int window = 0;
-	private int port = 25000;
-	private string serverPassword;
-	private string serverName;
-	private string serverComment;
-	private bool startServer = false;
+	private string roomPassword;
+	private string roomName = "RoomName";
+	private string roomComment;
 	public GUISkin skin;
+	private TypedLobby lobbyName = new TypedLobby("New_Lobby", LobbyType.Default);
+	private RoomInfo[] roomsList;
 
-	// Use this for initialization
 	void Start () 
 	{
-		MasterServer.ipAddress = "127.0.0.1";
-		MasterServer.port = 23466;
-		Network.natFacilitatorIP = "127.0.0.1";
-		Network.natFacilitatorPort = 50005;
+		PhotonNetwork.ConnectUsingSettings("v4.2");
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void Update () 
+	{
+
 	}
-		
-	void OnGUI () 
+
+	void OnGUI()
 	{
 		GUI.skin = skin;
 
-		// Main menu
 		if (window == 0) 
 		{
-			if (GUI.Button (new Rect (Screen.width * 0.45f, Screen.height * 0.35f, Screen.width * 0.15f, Screen.height * 0.10f), "Создать комнату")) 
+			if (GUI.Button (new Rect (Screen.width * 0.35f, Screen.height * 0.35f, Screen.width * 0.30f, Screen.height * 0.10f), "Создать комнату")) 
 			{
 				window = 1;
 			}
-			if (GUI.Button (new Rect (Screen.width * 0.45f, Screen.height * 0.50f, Screen.width * 0.15f, Screen.height * 0.10f), "Присоединиться к комнате")) 
+			if (GUI.Button (new Rect (Screen.width * 0.35f, Screen.height * 0.50f, Screen.width * 0.30f, Screen.height * 0.10f), "Присоединиться к комнате")) 
 			{
 				window = 2;
-		}
+			}
 			if (GUI.Button (new Rect (Screen.width * 0.80f, Screen.height * 0.05f, Screen.width * 0.15f, Screen.height * 0.10f), "Выход")) 
 			{
 				Application.Quit ();
@@ -50,31 +44,26 @@ public class MainMenu : MonoBehaviour
 		// Create room
 		if (window == 1) 
 		{
-			//port = GUI.TextField (new Rect (Screen.width / 2 + 15, Screen.height / 2 - 100, 85, 25), port);
-
 			GUI.Label (new Rect (Screen.width * 0.50f, Screen.height * 0.25f, Screen.width * 0.15f, Screen.height * 0.12f), "Создание комнаты");
 
 			GUI.Label (new Rect (Screen.width * 0.40f, Screen.height * 0.40f, Screen.width * 0.15f, Screen.height * 0.12f), "Название");
-			serverName = GUI.TextField (new Rect (Screen.width * 0.50f, Screen.height * 0.39f, Screen.width * 0.15f, Screen.height * 0.05f), ""+serverName);
+			roomName = GUI.TextField (new Rect (Screen.width * 0.50f, Screen.height * 0.39f, Screen.width * 0.15f, Screen.height * 0.05f), ""+roomName);
 
 			GUI.Label (new Rect (Screen.width * 0.40f, Screen.height * 0.50f, Screen.width * 0.15f, Screen.height * 0.12f), "Пароль");
-			serverPassword = GUI.TextField (new Rect (Screen.width * 0.50f, Screen.height * 0.49f, Screen.width * 0.15f, Screen.height * 0.05f), ""+serverPassword);
+			roomPassword = GUI.TextField (new Rect (Screen.width * 0.50f, Screen.height * 0.49f, Screen.width * 0.15f, Screen.height * 0.05f), ""+roomPassword);
 
 			GUI.Label (new Rect (Screen.width * 0.40f, Screen.height * 0.60f, Screen.width * 0.15f, Screen.height * 0.12f), "Описание");
-			serverComment = GUI.TextField (new Rect (Screen.width * 0.50f, Screen.height * 0.59f, Screen.width * 0.15f, Screen.height * 0.05f), ""+serverComment);
+			roomComment = GUI.TextField (new Rect (Screen.width * 0.50f, Screen.height * 0.59f, Screen.width * 0.15f, Screen.height * 0.05f), ""+roomComment);
 
 			if (GUI.Button (new Rect (Screen.width * 0.05f, Screen.height * 0.05f, Screen.width * 0.15f, Screen.height * 0.12f), "Назад")) 
 			{
 				window = 0;
 			}
-			if (startServer == false) 
+			if (PhotonNetwork.room == null) 
 			{
 				if (GUI.Button (new Rect (Screen.width * 0.80f, Screen.height * 0.80f, Screen.width * 0.15f, Screen.height * 0.12f), "Создать"))
 				{
-					Network.incomingPassword = serverPassword;
-					Network.InitializeServer (3, port, true);
-					MasterServer.RegisterHost("Rooms", serverName, serverComment);
-					startServer = true;
+					PhotonNetwork.CreateRoom(roomName, new RoomOptions() { maxPlayers = 3, isOpen = true, isVisible = true}, lobbyName);
 					window = 3;
 				}
 			}
@@ -84,42 +73,57 @@ public class MainMenu : MonoBehaviour
 		//Connect to room
 		if (window == 2) 
 		{
-			
+
 			if (GUI.Button (new Rect (Screen.width * 0.80f, Screen.height * 0.80f, Screen.width * 0.15f, Screen.height * 0.12f), "Назад"))
 			{
 				window = 0;
 			}
+				
 
-			if (GUI.Button (new Rect (Screen.width * 0.80f, Screen.height * 0.05f, Screen.width * 0.15f, Screen.height * 0.10f), "Обновить")) 
+			if (roomsList != null) 
 			{
-				MasterServer.ClearHostList ();
-				MasterServer.RequestHostList("Rooms");
-			}
-
-			HostData[] hostData = MasterServer.PollHostList ();
-			for(int i = 0; i<hostData.Length; i++)
-			{
-				GUI.Label(new Rect (Screen.width * 0.05f, Screen.height * 0.05f + i*25, Screen.width * 0.15f, Screen.height * 0.12f), hostData[i].gameName);
-				GUI.Label(new Rect (Screen.width * 0.25f, Screen.height * 0.05f + i*25, Screen.width * 0.15f, Screen.height * 0.12f), hostData[i].comment);
-				GUI.Label(new Rect (Screen.width * 0.50f, Screen.height * 0.05f + i*25, Screen.width * 0.15f, Screen.height * 0.12f), hostData[i].connectedPlayers+"/"+hostData[i].playerLimit);
-				if (hostData [i].passwordProtected == true) 
+				for (int i = 0; i < roomsList.Length; i++) 
 				{
-					GUI.Label(new Rect (Screen.width * 0.65f, Screen.height * 0.05f + i*25, Screen.width * 0.15f, Screen.height * 0.12f), "с паролем");
+					if (GUI.Button(new Rect(Screen.width * 0.10f, Screen.height * 0.10f + (110 * i), Screen.width * 0.15f, Screen.height * 0.12f), "Join " + roomsList[i].name)) 
+					{
+						PhotonNetwork.JoinRoom(roomsList[i].name);
+						window = 3;
+					}
 				}
-				GUI.Label(new Rect (Screen.width * 0.65f, Screen.height * 0.05f + i*25, Screen.width * 0.15f, Screen.height * 0.12f), "без пароля");
 			}
+
 		}
 
 		//Room for host
 		if (window == 3) 
 		{
 
-			if (GUI.Button (new Rect (Screen.width * 0.05f, Screen.height * 0.05f, Screen.width * 0.15f, Screen.height * 0.12f), "Удалить комнату"))
+			if (GUI.Button (new Rect (Screen.width * 0.05f, Screen.height * 0.05f, Screen.width * 0.15f, Screen.height * 0.12f), "Покинуть комнату"))
 			{
-				startServer = false;
-				Network.Disconnect ();
+				PhotonNetwork.LeaveRoom ();
+				window = 0;
 			}
-		}
-	
+		}			
+	}
+
+	void OnConnectedToMaster() 
+	{
+		PhotonNetwork.JoinLobby(lobbyName);
+	}
+
+	void OnReceivedRoomListUpdate()
+	{
+		Debug.Log ("Room was created");
+		roomsList = PhotonNetwork.GetRoomList();
+	}
+
+	void OnJoinedLobby () 
+	{
+		Debug.Log ("Joined Lobby");
+	}
+
+	void OnJoinedRoom ()
+	{
+		Debug.Log("Connected to Room");
 	}
 }
